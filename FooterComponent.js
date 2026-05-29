84bee454245820742c0d37c82e2f25f29bde4b4f
Byron P <42457662+orbitblob@users.noreply.github.com>
2026-05-29 08:43:50 -0500

    v1.2

---
+++ FooterComponent.js
@@ -0,0 +1,56 @@
+// FooterComponent.js
+class NNGFooter extends HTMLElement {
+    // This function automatically runs when the element hits the screen
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="bg-garden text-white lead py-4 mt-4">
+            <div class="container">
+                
+                <div class="row text-center mb-2">
+                    <div class="col-12">Let's work together!</div>
+                </div>
+                
+                <div class="row justify-content-center mb-2">
+                    <div class="col-6">
+                        <hr class="text-white opacity-100 my-0">
+                    </div>
+                </div>
+                
+                <div class="row justify-content-center text-center mb-2">
+                    
+                    <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+                        <div class="row">
+                            
+                            <div class="col-12 col-lg-6 mb-3 mb-lg-0">
+                                <i class="bi bi-envelope"></i> example@domain.com<br>
+                                <i class="bi bi-telephone"></i> (123) 456-7890
+                            </div>
+                            
+                            <div class="col-12 col-lg-6">
+                                <i class="bi bi-instagram"></i> @instahandle<br>
+                                <i class="bi bi-facebook"></i> /fbname
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                <div class="row justify-content-center mb-3">
+                    <div class="col-6">
+                        <hr class="text-white opacity-100 my-0">
+                    </div>
+                </div>
+                
+                <div class="row text-center small text-white-50">
+                    <div class="col-12">&copy; 2026 Native Nano Gardens</div>
+                </div>
+                
+            </div>
+        </footer>
+        `;
+    }
+}
+
+// Register this new custom HTML element tag with the browser
+customElements.define('nng-footer', NNGFooter);
