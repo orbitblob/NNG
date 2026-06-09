b56446e73c976beb238bf5d0143d938b2c2bd4d9
Byron P <42457662+orbitblob@users.noreply.github.com>
2026-06-09 12:24:35 -0500

    live v1.23

--- FooterComponent.js
+++ FooterComponent.js
@@ -27,7 +27,7 @@ class NNGFooter extends HTMLElement {
                             </div>
                             
                             <div class="col-12 col-lg-6">
-                                <i class="bi bi-instagram"></i><a href="https://www.instagram.com/native.nano.gardens/">@native.nano.gardens</a><br>
+                                <i class="bi bi-instagram"></i> <a href="https://www.instagram.com/native.nano.gardens/">@native.nano.gardens</a><br>
                                 <i class="bi bi-facebook"></i> <a href="https://www.facebook.com/profile.php?id=61584079015119">Native Nano Gardens</a>
                             </div>
                             
